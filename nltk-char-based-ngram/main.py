749960656fe837d9d397ef12c4f65b9594ea50b1
fhfirdausi <farizhfirdausi@gmail.com>
2024-01-26 00:33:23 -0600

    4gram + ACTUAL working perplexity

--- nltk-char-based-ngram/main.py
+++ nltk-char-based-ngram/main.py
@@ -6,7 +6,7 @@ from nltk.util import *
 TEST = "corpi/test.txt"
 TRAIN = "corpi/train.txt"
 
-NGRAM_ORDER = 2
+NGRAM_ORDER = 4
 
 def train_model(path_to_save: str):
     text = []
@@ -27,15 +27,19 @@ def perplexity(model: Laplace, path_to_file: str):
     with open(path_to_file, "r", encoding="utf-8") as test_file:
         for line in test_file:
             line = line.strip()
-            chars = pad_sequence([*line], n=NGRAM_ORDER, pad_left=True, pad_right=True, left_pad_symbol='<s>', right_pad_symbol='</s>')
-            gram.append(list(bigrams(chars)))
-    
+            chars = [*line]
+            ngram = ngrams(chars, NGRAM_ORDER, pad_left=True, left_pad_symbol="<s>", pad_right=True, right_pad_symbol="</s>")
+            gram.extend(ngram)
+    #print(gram[0])
+
     return model.perplexity(gram)
 
 if __name__ == '__main__':
-    train_model('models/laplace-bigram.pkl')
+    # train_model('models/laplace-4gram.pkl')
 
-    with open('models/laplace-bigram.pkl', 'rb') as fin:
+    with open('models/laplace-4gram.pkl', 'rb') as fin:
         model = pickle.load(fin)
+        print(len(model.vocab))
         print("".join(model.generate(30)))
-        print(perplexity(model, TEST))
+        print("train perplexity", perplexity(model, TRAIN))
+        print("test  perplexity", perplexity(model, TEST))
